f7c077e8a373a6d02d530690db1a60365e1c2982
TRDCOIN <tradecointoken@gmail.com>
2025-04-07 02:49:42 +0530

    updated DataManager statement

--- data-service/classes/DataManager.ts
+++ data-service/classes/DataManager.ts
@@ -94,7 +94,7 @@ export class DataManager {
             .element(document.body).injector().get('configService').get('GATEWAYS_SOON') || [];
 
         const descriptionHash = {
-            WAVES: { en: 'Waves is a blockchain ecosystem that offers comprehensive and effective blockchain-based tools for businesses, individuals and developers. Waves Platform offers unprecedented throughput and flexibility. Features include the LPoS consensus algorithm, Waves-NG protocol and advanced smart contract functionality.' }
+            WAVES: { en: 'KSS is a blockchain ecosystem that offers comprehensive and effective blockchain-based tools for businesses, individuals and developers. KSS Platform offers unprecedented throughput and flexibility. Features include the LPoS consensus algorithm, Waves-NG protocol and advanced smart contract functionality.' }
         };
 
         const gatewayAsset = {
