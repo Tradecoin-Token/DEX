6a4de02b42c422244bc47dea534b32248b715c64
TRDCOIN <tradecointoken@gmail.com>
2025-04-07 18:59:24 +0530

    no need

--- dist/desktop/mainnet/transfer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./main");
-function transfer(message, data = Object.create(null)) {
-    return main_1.main.bridge.transfer(message, data);
-}
-exports.transfer = transfer;
-//# sourceMappingURL=transfer.js.map
